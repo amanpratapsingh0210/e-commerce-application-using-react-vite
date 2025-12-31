06bf5fcc14974d02b584df598efb1f0c4fa06bb9
AmanPratapSingh <amanp.singh0210@gmail.com>
2025-12-31 11:12:44 +0530

    added cartSlice.js

---
+++ shoppy-globe/src/redux/cartSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    addToCart: (state, action) => {
+      const itemExists = state.items.find((item) => item.id === action.payload.id);
+      if (itemExists) {
+        itemExists.quantity += 1;
+      } else {
+        state.items.push({ ...action.payload, quantity: 1 });
+      }
+    },
+    removeFromCart: (state, action) => {
+      state.items = state.items.filter((item) => item.id !== action.payload);
+    },
+    updateQuantity: (state, action) => {
+      const { id, quantity } = action.payload;
+      const item = state.items.find((item) => item.id === id);
+      if (item && quantity > 0) {
+        item.quantity = quantity;
+      }
+    },
+    clearCart: (state) => {
+      state.items = [];
+    }
+  },
+});
+
+export const { addToCart, removeFromCart, updateQuantity, clearCart } = cartSlice.actions;
+export default cartSlice.reducer;
